03bc96ea41c2dfc67c3eb2bc43db8e0a2168d597
tranthanhtruc12 <thanhtruct322@gmail.com>
2023-10-25 19:12:30 +0700

    init project

---
+++ buoi5/form/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace form
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            txtYourName = new TextBox();
+            txtYear = new TextBox();
+            btnShow = new Button();
+            btnClear = new Button();
+            btnExit = new Button();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(94, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Your name";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(94, 173);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Year of birth";
+            // 
+            // txtYourName
+            // 
+            txtYourName.Location = new Point(246, 53);
+            txtYourName.Name = "txtYourName";
+            txtYourName.Size = new Size(308, 27);
+            txtYourName.TabIndex = 2;
+            // 
+            // txtYear
+            // 
+            txtYear.Location = new Point(246, 173);
+            txtYear.Name = "txtYear";
+            txtYear.Size = new Size(308, 27);
+            txtYear.TabIndex = 3;
+            // 
+            // btnShow
+            // 
+            btnShow.Location = new Point(149, 283);
+            btnShow.Name = "btnShow";
+            btnShow.Size = new Size(94, 39);
+            btnShow.TabIndex = 4;
+            btnShow.Text = "Show";
+            btnShow.UseVisualStyleBackColor = true;
+            btnShow.Click += btnShow_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(319, 283);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(97, 39);
+            btnClear.TabIndex = 5;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnExit
+            // 
+            btnExit.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            btnExit.Location = new Point(487, 283);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(101, 39);
+            btnExit.TabIndex = 6;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnExit);
+            Controls.Add(btnClear);
+            Controls.Add(btnShow);
+            Controls.Add(txtYear);
+            Controls.Add(txtYourName);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosing += Form1_FormClosing;
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtYourName;
+        private TextBox txtYear;
+        private Button btnShow;
+        private Button btnClear;
+        private Button btnExit;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ buoi5/form/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.ComponentModel;
+
+using System.Data;
+
+using System.Drawing;
+
+using System.Text;
+
+using System.Windows.Forms;
+
+using System.Text.RegularExpressions;
+
+namespace form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void txtYourName_Leave(object sender, EventArgs e)
+        {
+            Control ctr = (Control)sender;
+            if (ctr.Text.Trim().Length == 0)
+            {
+                this.errorProvider1.SetError(txtYourName, "You must enter Your Name!");
+            }
+            else
+            {
+                this.errorProvider1.Clear();
+            }
+        }
+
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            string s;
+            int age;
+            s = "Your name is: " + txtYourName.Text + "\n";
+            age = DateTime.Now.Year - Convert.ToInt32(txtYear.Text);
+            s = s + "Age: " + age.ToString();
+            MessageBox.Show(s);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult r;
+            r = MessageBox.Show("Bạn có muốn thoát?", "Thoát", MessageBoxButtons.YesNo
+                , MessageBoxIcon.Question, MessageBoxDefaultButton.Button3);
+            if (r == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtYourName.Clear();
+            txtYear.Clear();
+            txtYourName.Focus();
+        }
+    }
+}
